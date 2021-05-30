acc28e904a0ab9baaf82abff3482a02e622803a9
YuriZhuravlev <zhuravlevyuri2000@gmail.com>
2021-05-30 13:14:49 +0300

    Добавлены кнопки

--- app/src/main/java/com/zhuravlev/calculator/ui/CalculateView.kt
+++ app/src/main/java/com/zhuravlev/calculator/ui/CalculateView.kt
@@ -1,12 +1,13 @@
 package com.zhuravlev.calculator.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.material.Button
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 
@@ -22,6 +23,7 @@ fun CalculatorView(calculatorViewModel: CalculateViewModel) {
                 outputString
             )
         }
+        Keyboard()
         Button(onClick = {
             calculatorViewModel.viewModelScope.launch {
                 calculatorViewModel.input('0')
@@ -29,6 +31,64 @@ fun CalculatorView(calculatorViewModel: CalculateViewModel) {
         }) {
             Text(text = "0")
         }
+    }
+}
+
+@Composable
+fun Keyboard() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        Row(Modifier.weight(1f, true)) {
+            ButtonInput('C', modifier = Modifier.weight(1f))
+            ButtonInput('÷', modifier = Modifier.weight(1f))
+            ButtonInput('×', modifier = Modifier.weight(1f))
+            ButtonInput('D', modifier = Modifier.weight(1f)) // TODO DELETE
+        }
+        Row(Modifier.weight(1f, true)) {
+            ButtonInput('7', modifier = Modifier.weight(1f))
+            ButtonInput('8', modifier = Modifier.weight(1f))
+            ButtonInput('9', modifier = Modifier.weight(1f))
+            ButtonInput('-', modifier = Modifier.weight(1f))
+        }
+        Row(Modifier.weight(1f, true)) {
+            ButtonInput('4', modifier = androidx.compose.ui.Modifier.weight(1f))
+            ButtonInput('5', modifier = androidx.compose.ui.Modifier.weight(1f))
+            ButtonInput('6', modifier = androidx.compose.ui.Modifier.weight(1f))
+            ButtonInput('+', modifier = androidx.compose.ui.Modifier.weight(1f))
+        }
+        Row(Modifier.weight(1f, true)) {
+            ButtonInput('1', modifier = Modifier.weight(1f))
+            ButtonInput('2', modifier = Modifier.weight(1f))
+            ButtonInput('3', modifier = Modifier.weight(1f))
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            ) {}
+        }
+        Row(Modifier.weight(1f, true)) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            ) {}
+            ButtonInput('0', modifier = Modifier.weight(1f))
+            ButtonInput('.', modifier = Modifier.weight(1f))
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .weight(1f)
+            ) {}
+        }
+    }
+}
+
+@Composable
+fun ButtonInput(c: Char, modifier: Modifier = Modifier) {
+    OutlinedButton(
+        modifier = modifier.fillMaxSize(), onClick = { },
+        shape = RectangleShape
+    ) {
+        Text("$c")
     }
 }
 
